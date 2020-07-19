80d06678ef1baf11fb0a49aa3d2fc6c4975286cc
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.home>
2020-07-19 15:37:07 -0400

    add axios to get api data

--- logan-glass-family/src/App.vue
+++ logan-glass-family/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Family Picture 2019" src="./assets/family2019.jpg">
+    <Users msg="Family Listing:"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from './components/Users.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Users
   }
 }
 </script>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
 </style>
