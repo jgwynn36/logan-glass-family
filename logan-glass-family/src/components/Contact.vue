64a83026e4c3fbb37777af6eccc6b0674c0b58bd
Jacqueline Gwynn <jacquelinegwynn@jacquelines-air.home>
2020-07-19 16:57:15 -0400

    add email js and contact page

---
+++ logan-glass-family/src/components/Contact.vue
@@ -0,0 +1,43 @@
+<template>
+  <form class="contact-form" @submit.prevent="sendEmail">
+    <h1>Contact Form</h1>
+    <h3>Please fill out the form below and the committee will respond within 48 hours.  Thanks for you submission!!</h3>
+    <label>Name</label>
+    <input type="text" name="user_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
+</template>
+
+<script>
+
+import emailjs from 'emailjs-com'
+
+export default {
+  methods: {
+    sendEmail: (e) => {
+      emailjs.sendForm('LG_Form', 'contact_form', e.target, 'user_4BAboX5W7AjicClLYeJGA')
+        .then((result) => {
+          console.log('SUCCESS!', result.status, result.text)
+        }, (error) => {
+          console.log('FAILED...', error)
+        })
+    }
+  }
+}
+</script>
+<style>
+.contact-form {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 10px 10px 200px;
+}
+label {
+  padding: 30px
+}
+body {
+  background-color: #ffffe0;
+}
+</style>
--- logan-glass-family/src/main.js
+++ logan-glass-family/src/main.js
@@ -3,9 +3,20 @@ import 'bootstrap-css-only/css/bootstrap.min.css'
 import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import moment from 'moment'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+Vue.use(require("vue-moment"));
+
+Vue.prototype.moment = moment;
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
